79c6bf151b324523b481e5f44537451fbc923436
shee1998 <marywanjikumburu1@gmail.com>
2019-12-16 10:50:47 +0300

    create tests to check on the behavior of the different classes

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Image, Location, Category
 
-# Create your tests here.
+class ImageTestClass(TestCase):
+  """
+  A test that checks the behavior of the image class
+  """
+  
+  def setUp(self):
+    pass
+  
+class LocationTestClass(TestCase):
+  """
+  A test that checks for the behavior of the location class
+  """
+  
+  def setUp(self):
+    pass
+
+class CategoryTestClass(TestCase):
+  """
+  A test that checks on the behavior of the category class
+  """
+  
+  def setUp(self):
+    pass
